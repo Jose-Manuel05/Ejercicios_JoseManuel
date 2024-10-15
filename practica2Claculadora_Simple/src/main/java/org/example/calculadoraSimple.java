3ca1f356db53dd0170f3eea29c38ec015d7022f5
JOSE-LAPTOP\josem <josemanuel05102001@gmail.com>
2024-10-15 20:40:28 +0200

    no message

--- practica2Claculadora_Simple/src/main/java/org/example/calculadoraSimple.java
+++ practica2Claculadora_Simple/src/main/java/org/example/calculadoraSimple.java
@@ -14,50 +14,60 @@ public class calculadoraSimple {
 
         int num1 = 0;
         int num2 = 0;
+        String op = "";
+
+        boolean opValido = false;
 
         try {
 
-            System.out.println("Introduce un numero: ");
+            System.out.println("Introduce el primer numero: ");
             num1 = teclado.nextInt();
 
             System.out.println("Introduce el operando (+, -, *, /, r (para la raiz cuadrada)): ");
-            String op = teclado.next();
+            op = teclado.next();
 
-            if (op.equals("r")) {
-                if (num1 < 0) {
-                    System.out.println("Error, no puedes introducir un numero negativo");
-                }else {
-                    double resultado = Math.sqrt(num1);
-                    System.out.println("La raiz cuadrade de " +num1+ " es: " +resultado);
-                }
+            System.out.println("Introduce el segundo numero: ");
+            num2 = teclado.nextInt();
 
+
+        }catch (InputMismatchException e){
+            System.out.println("Error, introduce un valor numerico correcto. " + e.getMessage());
+        }
+
+        if (op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/") || op.equals("r")) {
+            opValido = true;
+        } else {
+            System.out.println("Erro, operador no válido.");
+        }
+
+        if (op.equals("r")) {
+            if (num1 < 0) {
+                System.out.println("Error, no puedes introducir un numero negativo");
             }else {
-                System.out.println("Introduce el segundo numero: ");
-                num2 = teclado.nextInt();
-
-                switch (op) {
-
-                    case "+":
-                        System.out.println("Resultado: " + (num1 + num2));
-                        break;
-                    case "-":
-                        System.out.println("Resultado: " + (num1 - num2));
-                        break;
-                    case "*":
-                        System.out.println("Resultado: " + (num1 * num2));
-                    case "/":
-                        if (num1 == 0) {
-                            System.out.println("Error: No se puede dividir por cero.");
-                        } else {
-                            System.out.println("Resultado: " + (num1 / num2));
-                        }
-                        break;
-                }
+                double resultado = Math.sqrt(num1);
+                System.out.println("La raiz cuadrade de " +num1+ " es: " +resultado);
             }
-        }catch (InputMismatchException e1){
-            System.out.println("Error, introduce un valor numerico correcto. " + e1.getMessage());
-        }catch (ArithmeticException e2){
-            System.out.println("Error, introduce un valor. " + e2.getMessage());
+
+        }else {
+
+            switch (op) {
+
+                case "+":
+                    System.out.println("Resultado: " + (num1 + num2));
+                    break;
+                case "-":
+                    System.out.println("Resultado: " + (num1 - num2));
+                    break;
+                case "*":
+                    System.out.println("Resultado: " + (num1 * num2));
+                case "/":
+                    if (num1 == 0|| num2 == 0) {
+                        System.out.println("Error: No se puede dividir por cero.");
+                    } else {
+                        System.out.println("Resultado: " + (num1 / num2));
+                    }
+                    break;
+            }
         }
 
 
